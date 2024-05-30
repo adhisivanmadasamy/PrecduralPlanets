a3ac92a5a6f447d6a488971f9463387930d2b436
Adhisivan Madasamy <adhi2000uk@gmail.com>
2024-05-30 11:35:26 +0100

    Initial commit - 60% project done
    Committing after 60% of project completed:
    - Creating sphere mesh with adjustable resolution.
    - Adding different layers of noise to generate continents, rocky areas and ridges.
    - converted project to URP
    - created custom shader to color the terrain based on their height value

---
+++ Assets/Scripts/NoiseSettings.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+[System.Serializable]
+public class NoiseSettings 
+{
+    
+    
+
+    public enum FilterType { Simple, Rigid };
+    public FilterType filterType;
+
+    [ConditionalHide("filterType",0)]
+    public SimpleNoiseSettings simpleNoiseSettings;
+
+    [ConditionalHide("filterType", 1)]
+    public RigidNoiseSettings rigidNoiseSettings;
+
+    [System.Serializable]
+    public class SimpleNoiseSettings
+    {
+        [Range(1, 8)]
+        public int numLayers = 1;
+        public float baseRoughness = 1;
+        public float persistence = 0.5f;
+
+        public float strength = 1;
+        public float roughness = 2;
+        public Vector3 centre;
+
+        public float minValue;
+    }
+
+    [System.Serializable]
+    public class RigidNoiseSettings : SimpleNoiseSettings
+    {
+        public float weightMultiplier = 0.8f;
+    }
+    
+
+    
+}
